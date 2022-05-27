37ee214a857c697964938cf99533c4caa8f0f447
Yusuf Fahrul <yusuffahrul.10@gmail.com>
2022-05-27 14:55:52 +0700

    penambahan fungsi

---
+++ fungsi_parameter.py
@@ -0,0 +1,17 @@
+def selamat_datang(nama):
+    
+    print('hello ', nama, " apa kabar?")
+
+
+selamat_datang('budi')
+
+
+def greetings(nama, waktu):
+    print('hello', nama, 'selamat', waktu)
+    print('apakah ', nama, 'sudah datang', waktu, 'ini?')
+
+
+greetings('andi', 'pagi')
+greetings('rudi', 'siang')
+greetings('wahyu', 'malam')
+
